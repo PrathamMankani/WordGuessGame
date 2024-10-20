76166dffec2cb8c482e4a7f89c5a8c8feac4df40
PrathamMankani <149860685+PrathamMankani@users.noreply.github.com>
2024-10-20 21:40:50 +0530

    Update game.c

--- game.c
+++ game.c
@@ -3,33 +3,63 @@
 #include <stdlib.h>
 #include <time.h>
 
-void shuffle(char *word) {
-    for (int i = strlen(word) - 1; i > 0; i--) {
+// Function to swap two characters
+void swap(char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to shuffle the letters of a word (scramble)
+void shuffleWord(char *word) {
+    int len = strlen(word);
+    for (int i = len - 1; i > 0; i--) {
+        // Generate a random index
         int j = rand() % (i + 1);
-        char temp = word[i];
-        word[i] = word[j];
-        word[j] = temp;
+        // Swap the characters at i and j
+        swap(&word[i], &word[j]);
     }
 }
 
 int main() {
-    const char *words[] = {"programming", "engineer", "computer", "data", "science", "algorithm"};
+    // Predefined list of words
+    const char *wordList[] = {"programming", "engineer", "computer", "data", "science", "algorithm"};
+    int wordCount = sizeof(wordList) / sizeof(wordList[0]);
+    
+    // Initialize random number generator
     srand(time(0));
-    const char *word = words[rand() % (sizeof(words) / sizeof(words[0]))];
-    char scrambled[100];
-    strcpy(scrambled, word);
-    shuffle(scrambled);
 
-    printf("Scrambled: %s\n", scrambled);
+    // Pick a random word from the list
+    int randomIndex = rand() % wordCount;
+    const char *selectedWord = wordList[randomIndex];
+    
+    // Create a copy of the selected word to scramble it
+    char scrambledWord[100];
+    strcpy(scrambledWord, selectedWord);
+    
+    // Scramble the selected word
+    shuffleWord(scrambledWord);
+
+    printf("Welcome to the Word Scramble Game!\n");
+    printf("Here is your scrambled word: %s\n", scrambledWord);
 
     char guess[100];
     int attempts = 0;
-    do {
-        printf("Guess: ");
+
+    // Allow the player to guess the word
+    while (1) {
+        printf("Enter your guess: ");
         scanf("%s", guess);
         attempts++;
-    } while (strcmp(guess, word) != 0);
 
-    printf("Correct! Attempts: %d\n", attempts);
-    return 0;
+        // Check if the guessed word matches the original word
+        if (strcmp(guess, selectedWord) == 0) {
+            printf("Congratulations! You guessed the word correctly in %d attempts.\n", attempts);
+            break;
+        } else {
+            printf("Incorrect guess. Try again!\n");
+        }
+    }
+
+    return 0;
 }
